5f31aa0a77883ea7704ea6e9915905b38e13d0d5
spandana <spandana.bodapati@gmail.com>
2020-06-23 18:42:16 -0500

    Added logged stmts

--- src/main/java/com/example/application/SpringGitJenkinsIntegrationApplication.java
+++ src/main/java/com/example/application/SpringGitJenkinsIntegrationApplication.java
@@ -1,12 +1,24 @@
 package com.example.application;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitJenkinsIntegrationApplication {
 
+	private static Logger log = LoggerFactory.getLogger(SpringGitJenkinsIntegrationApplication.class);
+
+	@PostConstruct
+	public void init() {
+		log.info("Application Started");
+	}
+
 	public static void main(String[] args) {
+		log.info("Application executed");
 		SpringApplication.run(SpringGitJenkinsIntegrationApplication.class, args);
 	}
 
--- src/test/java/com/example/application/SpringGitJenkinsIntegrationApplicationTests.java
+++ src/test/java/com/example/application/SpringGitJenkinsIntegrationApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.application;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringGitJenkinsIntegrationApplicationTests {
+	
+	private static Logger log = LoggerFactory.getLogger(SpringGitJenkinsIntegrationApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		log.info("Application Tested");
 	}
 
 }
